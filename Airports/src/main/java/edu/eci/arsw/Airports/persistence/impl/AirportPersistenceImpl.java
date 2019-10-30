9dfe56c09146add683251bf68c3559a7fabc0348
Natalia Palacios <nataplc24@gmail.com>
2019-10-30 09:46:43 -0500

    Ya muestra

--- Airports/src/main/java/edu/eci/arsw/Airports/persistence/impl/AirportPersistenceImpl.java
+++ Airports/src/main/java/edu/eci/arsw/Airports/persistence/impl/AirportPersistenceImpl.java
@@ -18,10 +18,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class AirportPersistenceImpl implements AirportPersistence{
-	//@Singleton
-    //@Autowired
+	
     private ConcurrentHashMap<String, Airport> aeropuertos = new ConcurrentHashMap<>();
-	//@Singleton
     private ConcurrentHashMap<String, Long> tiempo =  new ConcurrentHashMap<>();
     private static final String USER_AGENT = "";
 
